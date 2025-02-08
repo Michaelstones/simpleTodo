fae17977b24a4f088bf1fbf821ada709c5c84a58
michaelstones <michaelshow99@gmail>
2025-02-08 15:03:41 +0100

    upload

---
+++ lib/view/tasks/component/date_selection_picker.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class DateTimeSelection extends StatelessWidget {
+  const DateTimeSelection({
+    super.key,
+    required this.onTap,
+    required this.title,
+  });
+  final VoidCallback onTap;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          width: double.infinity,
+          height: 55,
+          margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(color: Colors.grey.shade300)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Text(
+                  title,
+                  // style: textTheme.headlineMedium,
+                ),
+              ),
+              Container(
+                height: 35,
+                width: 80,
+                margin: EdgeInsets.only(right: 10),
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade100,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Center(
+                  child: Text(title),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
--- lib/view/tasks/task_view.dart
+++ lib/view/tasks/task_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cupertino_date_picker_fork/flutter_cupertino_date_picker_fork.dart';
 import 'package:todo/extensions/space_ext.dart';
+import 'package:todo/utils/app_colours.dart';
 import 'package:todo/utils/app_str.dart';
+import 'package:todo/view/tasks/component/date_selection_picker.dart';
 import 'package:todo/view/tasks/component/rep_txt_field.dart';
 import 'package:todo/view/tasks/widget/task_view_appbar.dart';
 
@@ -25,36 +28,101 @@ class _TaskViewState extends State<TaskView> {
           child: Column(
             children: [
               _buildTopSideText(textTheme),
-              SizedBox(
-                width: double.infinity,
-                height: 530,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 30),
-                      child: Text(
-                        AppString.titleOfTitleTextField,
-                        // style: textTheme.headlineMedium,
-                      ),
-                    ),
-                    RepTextField(controller: titleController),
-                    10.h,
-                    RepTextField(
-                      controller: descriptionController,
-                      isForDesc: true,
-                    ),
-                    Container(
-                      width: double.infinity,
-                      height: 55,
-                      decoration: BoxDecoration(),
-                    )
-                  ],
-                ),
-              )
+              _buildMainAction(titleController, descriptionController, context),
+              _buildButton()
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Padding _buildButton() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          MaterialButton(
+            minWidth: 150,
+            height: 55,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            color: Colors.white,
+            onPressed: () {},
+            child: Row(children: [
+              Icon(Icons.close, color: AppColours.primaryColor),
+              5.w,
+              Text(
+                AppString.deleteTask,
+                style: TextStyle(color: AppColours.primaryColor),
+              ),
+            ]),
+          ),
+          //
+          MaterialButton(
+            minWidth: 150,
+            height: 55,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            color: AppColours.primaryColor,
+            onPressed: () {},
+            child: Text(
+              AppString.addTaskString,
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  SizedBox _buildMainAction(TextEditingController titleController,
+      TextEditingController descriptionController, BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 530,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: 30),
+            child: Text(
+              AppString.titleOfTitleTextField,
+              // style: textTheme.headlineMedium,
+            ),
+          ),
+          RepTextField(controller: titleController),
+          10.h,
+          RepTextField(
+            controller: descriptionController,
+            isForDesc: true,
+          ),
+          DateTimeSelection(
+              onTap: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (_) => SizedBox(
+                          height: 280,
+                          child: TimePickerWidget(
+                            onChange: (_, __) {},
+                            dateFormat: 'HH:mm',
+                            onConfirm: (dateTime, _) {},
+                          ),
+                        ));
+              },
+              title: AppString.timeString),
+          DateTimeSelection(
+              onTap: () {
+                DatePicker.showDatePicker(
+                  context,
+                  minDateTime: DateTime.now(),
+                  maxDateTime: DateTime(2030, 4, 5),
+                  onConfirm: (dateTime, _) {},
+                );
+              },
+              title: AppString.dateString),
+        ],
       ),
     );
   }
